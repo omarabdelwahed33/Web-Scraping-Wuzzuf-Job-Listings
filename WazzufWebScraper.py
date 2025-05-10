1c138afd0ca65f7be8e5cc23a446cafb1705bc4d
omarabdelwahed33 <oabdelwahed605@gmail.com>
2025-05-10 23:36:48 +0300

    Update WazzufWebScraper.py

--- WazzufWebScraper.py
+++ WazzufWebScraper.py
@@ -1,41 +1,34 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 import csv
-
-url = "https://wuzzuf.net/search/jobs/?q=illustrator&a=navbg"
-client = urlopen(url)
-html = client.read()
-client.close()
-soup = bs(html, "html.parser")
-containers = soup.find_all("div", {'class': "css-1gatmva e1v1l3u10"})
+import time
 
 
-jtitle = containers[0].find_all("h2",{"class": "css-m604qf"})
+with open("C:/Users/DELL/Documents/web scrapinggg/wuzzuf.csv", "w", encoding="utf-8", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["job_title", "company_name", "job_type"])
 
-
-cname = containers[0].find_all("a", {"class": "css-17s97q8"})
-
-jtype = containers[0].find_all("span", {"class": "css-o1vzmt eoyjyou0"})
-
-
-
-f = open("C:/Users/DELL/Documents/web scrapinggg/wuzzuf.csv","w" ,encoding="utf-8")
-header = "job_title, company_name, jop_type\n"
-f.write(header)
-
-for container in containers :
-    jtitle = container.find_all("h2",{"class": "css-m604qf"})
-    job_title = jtitle[0].text.strip()
-
-    cname = container.find_all("a", {"class": "css-17s97q8"})
-    company_name = cname[0].text.strip()
     
-    jtype = container.find_all("span", {"class": "css-o1vzmt eoyjyou0"})
-    jop_type = jtype[0].text.strip()
-    
-    f.write(job_title + "," + company_name + "," + jop_type +  "\n")
+    for page in range(0, 50, 10):  
+        url = f"https://wuzzuf.net/search/jobs/?q=illustrator&a=navbg&start={page}"
+        client = urlopen(url)
+        html = client.read()
+        client.close()
 
-f.close()
+        soup = bs(html, "html.parser")
+        containers = soup.find_all("div", {'class': "css-1gatmva e1v1l3u10"})
+
+        for container in containers:
+            try:
+                job_title = container.find("h2", {"class": "css-m604qf"}).text.strip()
+                company_name = container.find("a", {"class": "css-17s97q8"}).text.strip()
+                job_type = container.find("span", {"class": "css-o1vzmt eoyjyou0"}).text.strip()
+                writer.writerow([job_title, company_name, job_type])
+            except AttributeError:
+                continue  
+        
+        time.sleep(1)
+
 
 
 
